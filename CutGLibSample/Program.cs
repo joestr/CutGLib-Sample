213c2bf28b2fce0e1a2583b4455d72505b96ccf9
Joel Strasser <j.strasser@leeb-balkone.com>
2023-03-22 17:01:09 +0100

    include small sample

--- CutGLibSample/Program.cs
+++ CutGLibSample/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace CutGLibSample
+﻿using CutGLib;
+
+namespace CutGLibSample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CutEngine cutEngine = new CutEngine();
+
+            cutEngine.AddLinearStock(ALength: 5000.0, aCount: 25);
+
+            cutEngine.AddLinearPart(ALength: 150.0, aCount: 4, aAngleStart: 45.0, aAngleEnd: 45.0, aID: "Part_1");
+            cutEngine.AddLinearPart(ALength: 250.0, aCount: 4, aAngleStart: 45.0, aAngleEnd: 45.0, aID: "Part_2");
+            cutEngine.AddLinearPart(ALength: 100.0, aCount: 4, aID: "Part_3");
+
+            cutEngine.SawWidth = 10.0;
+
+            Console.WriteLine(cutEngine.ExecuteLinear());
         }
     }
 }
